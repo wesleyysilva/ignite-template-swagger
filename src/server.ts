53665593a4414e73d1f3a256ec1ee02e74b0cb1e
Wesley Silva <wesleyysilva@yahoo.com.br>
2022-07-21 18:43:15 -0300

    finish

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUi from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "../swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
